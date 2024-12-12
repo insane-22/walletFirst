ec2f1e9eb0d45dfed422386fc26212a579be0bc0
insane-22 <bhumikabachchan11315@gmail.com>
2024-12-12 18:13:39 +0530

    tailwind.config fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
- 
-const colors = require("tailwindcss/colors");
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
 /** @type {import('tailwindcss').Config} */
 export default {
